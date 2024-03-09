66278376164f0511947363015335b7c30c43850a
kjm <minn951120@naver.com>
2024-03-09 15:51:41 +0900

    struct example

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,13 +5,18 @@ struct User {
     sign_in_count: u64
 }
 
-fn main() {
-    let user1 = User {
-        active: true,
-        name: "JM KIM".to_string(),
-        email: "jm@dunamu.com".to_string(),
-        sign_in_count: 1
-    };
+fn build_user(email: String, name: String) -> User {
+    User {
+        active: true, name, email, sign_in_count: 1
+    }
+}
 
-    println!("{}, {}, {}, {}", user1.active, user1.name, user1.email, user1.sign_in_count)
+fn main() {
+    let mut user1 = build_user("jm6534@gmail.com".to_string(), "KJM".to_string());
+    let user2: User = User { name: "kjm".to_string(), ..user1 };
+
+    user1.email = String::from("minn987@naver.com");
+
+    println!("{}, {}, {}, {}", user1.active, user1.name, user1.email, user1.sign_in_count);
+    println!("{}, {}, {}, {}", user2.active, user2.name, user2.email, user2.sign_in_count);
 }
